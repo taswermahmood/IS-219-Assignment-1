1038d0ffe87b4e013e493003b59897802fe4f2d3
Taswermahmood <tm379@njit.edu>
2020-02-23 23:39:41 -0500

    added

--- tests/square.test.js
+++ tests/square.test.js
@@ -1,5 +1,5 @@
 const square = require('../square');
 
 test('2 square 2  equal 4', () => {
-    expect(operations.square(2, 2)).toBe(4)
+    expect(square(2)).toBe(4);
 });
--- tests/squareRoot.test.js
+++ tests/squareRoot.test.js
@@ -1,5 +1,5 @@
 const squareRoot = require('../squareRoot');
 
 test('16 squareroot 4  equal 4', () => {
-    expect(operations.squareRoot(16)).toBe(4)
+    expect(squareRoot(16)).toBe(4);
 });
